8917c0ab6cfd61285fdd736b430ae8c59afa22d0
aeolus <108349270+aeolus87@users.noreply.github.com>
2024-05-23 14:04:37 +0800

    Update Home.jsx

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
     const { currentUser } = useAuth();
     
     return (
-        <div className="mx-0 my-0 px-0 py-4 bg-[#2f4b56] z-20">
+        <div className="h-screen mx-0 my-0 px-0 py-4 bg-[#2f4b56] z-20">
             <Fmap />
             <Map />
         </div>
