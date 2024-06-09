83000d0eea84176220c380191bb2c27be4cfc337
aeolus87 <callejo.julius.07222003@gmail.com>
2024-06-09 17:13:36 +0800

    zzz

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <App />
+    <SpeedInsights />
   </BrowserRouter>,
 );
