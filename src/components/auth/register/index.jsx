73cb0a42ec2c7ccaa3075b46eb03da3df1731834
aeolus87 <callejo.julius.07222003@gmail.com>
2024-06-09 10:36:54 +0800

    added a term and conds

--- src/components/auth/register/index.jsx
+++ src/components/auth/register/index.jsx
@@ -1,154 +1,202 @@
-import React, { useState } from 'react'
-import { Navigate, Link } from 'react-router-dom'
-import { useAuth } from '../../../contexts/authContext'
-import { doCreateUserWithEmailAndPassword } from '../../../firebase/auth'
+import React, { useState, useEffect } from 'react';
+import { Navigate, Link } from 'react-router-dom';
+import { useAuth } from '../../../contexts/authContext';
+import { doCreateUserWithEmailAndPassword } from '../../../firebase/auth';
 
 const Register = () => {
+  const [fullName, setFullName] = useState(
+    localStorage.getItem('fullName') || '',
+  ); // Load from localStorage
+  const [email, setEmail] = useState(localStorage.getItem('email') || '');
+  const [password, setPassword] = useState(
+    localStorage.getItem('password') || '',
+  );
+  const [confirmPassword, setConfirmPassword] = useState(
+    localStorage.getItem('confirmPassword') || '',
+  );
+  const [isRegistering, setIsRegistering] = useState(false);
+  const [emailErrorMessage, setEmailErrorMessage] = useState('');
+  const [passwordErrorMessage, setPasswordErrorMessage] = useState('');
+  const [confirmPasswordTouched, setConfirmPasswordTouched] = useState(false);
+  const [agreedToTerms, setAgreedToTerms] = useState(
+    localStorage.getItem('agreedToTerms') === 'true',
+  );
 
-    const [fullName, setFullName] = useState('') // State for full name
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [confirmPassword, setConfirmPassword] = useState('')
-    const [isRegistering, setIsRegistering] = useState(false)
-    const [emailErrorMessage, setEmailErrorMessage] = useState('')
-    const [passwordErrorMessage, setPasswordErrorMessage] = useState('')
-    const [confirmPasswordTouched, setConfirmPasswordTouched] = useState(false)
+  const { userLoggedIn } = useAuth();
 
-    const { userLoggedIn } = useAuth()
+  useEffect(() => {
+    localStorage.setItem('fullName', fullName);
+    localStorage.setItem('email', email);
+    localStorage.setItem('password', password);
+    localStorage.setItem('confirmPassword', confirmPassword);
+    localStorage.setItem('agreedToTerms', agreedToTerms);
+  }, [fullName, email, password, confirmPassword, agreedToTerms]);
 
-    const onSubmit = async (e) => {
-        e.preventDefault()
-        if(!isRegistering && password === confirmPassword) {
-            setIsRegistering(true)
-            try {
-                await doCreateUserWithEmailAndPassword(email, password, fullName)
-            } catch (error) {
-                if (error.code === 'auth/email-already-in-use') {
-                    setEmailErrorMessage('This email is already used')
-                } else {
-                    setPasswordErrorMessage('Error signing up. Please try again later.')
-                }
-                setIsRegistering(false)
-            }
-        }
-    }
-
-    const handlePasswordChange = (e) => {
-        setPassword(e.target.value)
-        if (confirmPasswordTouched && e.target.value !== confirmPassword) {
-            setPasswordErrorMessage('Passwords do not match')
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    if (!isRegistering && password === confirmPassword) {
+      setIsRegistering(true);
+      try {
+        await doCreateUserWithEmailAndPassword(email, password, fullName);
+      } catch (error) {
+        if (error.code === 'auth/email-already-in-use') {
+          setEmailErrorMessage('This email is already used');
         } else {
-            setPasswordErrorMessage('')
+          setPasswordErrorMessage('Error signing up. Please try again later.');
         }
+        setIsRegistering(false);
+      }
     }
+  };
 
-    const handleConfirmPasswordChange = (e) => {
-        setConfirmPassword(e.target.value)
-        if (e.target.value !== password) {
-            setPasswordErrorMessage('Passwords do not match')
-        } else {
-            setPasswordErrorMessage('')
-        }
-        setConfirmPasswordTouched(true)
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+    if (confirmPasswordTouched && e.target.value !== confirmPassword) {
+      setPasswordErrorMessage('Passwords do not match');
+    } else {
+      setPasswordErrorMessage('');
     }
+  };
 
-    return (
-        <>
-            {userLoggedIn && (<Navigate to={'/home'} replace={true} />)}
+  const handleConfirmPasswordChange = (e) => {
+    setConfirmPassword(e.target.value);
+    if (e.target.value !== password) {
+      setPasswordErrorMessage('Passwords do not match');
+    } else {
+      setPasswordErrorMessage('');
+    }
+    setConfirmPasswordTouched(true);
+  };
 
-            <main className="w-full h-screen flex self-center place-content-center place-items-center">
-                <div className="w-96 text-gray-600 space-y-5 p-4 shadow-xl border bg-[#fff9f9] rounded-xl">
-                    <div className="text-center mb-6">
-                        <div className="mt-2">
-                            <h3 className="text-gray-800 text-xl font-semibold sm:text-2xl">Create a New Account</h3>
-                        </div>
+  return (
+    <>
+      {userLoggedIn && <Navigate to={'/home'} replace={true} />}
 
-                    </div>
-                    <form
-                        onSubmit={onSubmit}
-                        className="space-y-4"
-                    >
-                        <div>
-                            <label className="text-sm text-gray-600 font-bold">
-                                Full Name
-                            </label>
-                            <input
-                                type="text"
-                                autoComplete='name'
-                                required
-                                value={fullName} onChange={(e) => setFullName(e.target.value)}
-                                className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:indigo-600 shadow-sm rounded-lg transition duration-300"
-                            />
-                        </div>
+      <main className="w-full h-screen flex self-center place-content-center place-items-center">
+        <div className="w-96 text-gray-600 space-y-5 p-4 shadow-xl border bg-[#fff9f9] rounded-xl">
+          <div className="text-center mb-6">
+            <div className="mt-2">
+              <h3 className="text-gray-800 text-xl font-semibold sm:text-2xl">
+                Create a New Account
+              </h3>
+            </div>
+          </div>
+          <form onSubmit={onSubmit} className="space-y-4">
+            <div>
+              <label className="text-sm text-gray-600 font-bold">
+                Full Name
+              </label>
+              <input
+                type="text"
+                autoComplete="name"
+                required
+                value={fullName}
+                onChange={(e) => setFullName(e.target.value)}
+                className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:indigo-600 shadow-sm rounded-lg transition duration-300"
+              />
+            </div>
 
-                        <div>
-                            <label className="text-sm text-gray-600 font-bold">
-                                Email
-                            </label>
-                            <input
-                                type="email"
-                                autoComplete='email'
-                                required
-                                value={email} onChange={(e) => { setEmail(e.target.value); setEmailErrorMessage('') }}
-                                className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:indigo-600 shadow-sm rounded-lg transition duration-300"
-                            />
-                            {emailErrorMessage && (
-                                <span className='text-red-600 text-sm lg:ml-2'>{emailErrorMessage}</span>
-                            )}
-                        </div>
+            <div>
+              <label className="text-sm text-gray-600 font-bold">Email</label>
+              <input
+                type="email"
+                autoComplete="email"
+                required
+                value={email}
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                  setEmailErrorMessage('');
+                }}
+                className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:indigo-600 shadow-sm rounded-lg transition duration-300"
+              />
+              {emailErrorMessage && (
+                <span className="text-red-600 text-sm lg:ml-2">
+                  {emailErrorMessage}
+                </span>
+              )}
+            </div>
 
-                        <div>
-                            <label className="text-sm text-gray-600 font-bold">
-                                Password
-                            </label>
-                            <input
-                                disabled={isRegistering}
-                                type="password"
-                                autoComplete='new-password'
-                                required
-                                value={password} onChange={handlePasswordChange}
-                                className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg transition duration-300"
-                            />
-                        </div>
+            <div>
+              <label className="text-sm text-gray-600 font-bold">
+                Password
+              </label>
+              <input
+                disabled={isRegistering}
+                type="password"
+                autoComplete="new-password"
+                required
+                value={password}
+                onChange={handlePasswordChange}
+                className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg transition duration-300"
+              />
+            </div>
 
-                        <div>
-                            <label className="text-sm text-gray-600 font-bold">
-                                Confirm Password
-                            </label>
-                            <input
-                                disabled={isRegistering}
-                                type="password"
-                                autoComplete='off'
-                                required
-                                value={confirmPassword} onChange={handleConfirmPasswordChange}
-                                className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg transition duration-300"
-                            />
-                        </div>
+            <div>
+              <label className="text-sm text-gray-600 font-bold">
+                Confirm Password
+              </label>
+              <input
+                disabled={isRegistering}
+                type="password"
+                autoComplete="off"
+                required
+                value={confirmPassword}
+                onChange={handleConfirmPasswordChange}
+                className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg transition duration-300"
+              />
+            </div>
 
-                        {passwordErrorMessage && confirmPasswordTouched && (
-                            <span className='text-red-600'>{passwordErrorMessage}</span>
-                        )}
+            <p className="text-gray-600 flex items-center">
+              <input
+                type="checkbox"
+                id="termsCheckbox"
+                className="mr-2 h-5 w-5"
+                checked={agreedToTerms} // Use the agreedToTerms state
+                onChange={() => setAgreedToTerms(!agreedToTerms)} // Update state on change
+                required
+              />
+              <label htmlFor="termsCheckbox">
+                By registering, you agree to our{' '}
+                <Link to="/terms" className="text-indigo-600 hover:underline">
+                  Terms and Conditions
+                </Link>
+                .
+              </label>
+            </p>
 
-                        {passwordErrorMessage && !confirmPasswordTouched && (
-                            <span className='text-red-600'>Please confirm your password</span>
-                        )}
+            {passwordErrorMessage && confirmPasswordTouched && (
+              <span className="text-red-600">{passwordErrorMessage}</span>
+            )}
 
-                        <button
-                            type="submit"
-                            disabled={isRegistering || emailErrorMessage !== '' || passwordErrorMessage !== ''}
-                            className={`w-full px-4 py-2 text-white font-medium rounded-lg ${isRegistering ? 'bg-gray-300 cursor-not-allowed' : 'bg-indigo-600 hover:bg-indigo-700 hover:shadow-xl transition duration-300'}`}
-                        >
-                            {isRegistering ? 'Signing Up...' : 'Sign Up'}
-                        </button>
-                        <div className="text-sm text-center">
-                            Already have an account? {'   '}
-                            <Link to={'/login'} className="text-center text-sm hover:underline font-bold">Continue</Link>
-                        </div>
-                    </form>
-                </div>
-            </main>
-        </>
-    )
-}
+            {passwordErrorMessage && !confirmPasswordTouched && (
+              <span className="text-red-600">Please confirm your password</span>
+            )}
 
-export default Register
+            <button
+              type="submit"
+              disabled={
+                isRegistering ||
+                emailErrorMessage !== '' ||
+                passwordErrorMessage !== ''
+              }
+              className={`w-full px-4 py-2 text-white font-medium rounded-lg ${isRegistering ? 'bg-gray-300 cursor-not-allowed' : 'bg-indigo-600 hover:bg-indigo-700 hover:shadow-xl transition duration-300'}`}
+            >
+              {isRegistering ? 'Signing Up...' : 'Sign Up'}
+            </button>
+            <div className="text-sm text-center">
+              Already have an account? {'   '}
+              <Link
+                to={'/login'}
+                className="text-center text-sm hover:underline font-bold"
+              >
+                Continue
+              </Link>
+            </div>
+          </form>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default Register;
---
+++ src/components/auth/terms/TermsConditions.jsx
@@ -0,0 +1,179 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const TermsAndConditions = () => {
+  const navigate = useNavigate();
+
+  const handleAgree = () => {
+    localStorage.setItem('agreedToTerms', 'true');
+    navigate('/register');
+  };
+
+  return (
+    <div className="container mx-auto my-16 px-4 py-8 bg-white text-gray-800 shadow-lg rounded-lg">
+      <h1 className="text-4xl font-bold mb-4">Terms and Conditions</h1>
+      <p className="mb-4">Last updated: June 9, 2024</p>
+
+      <section className="mb-6">
+        <h2 className="text-2xl font-semibold mb-2">1. Acceptance of Terms</h2>
+        <p>
+          By accessing and using the Service, you agree to be bound by these
+          Terms. If you disagree with any part of the Terms, then you do not
+          have permission to access the Service.
+        </p>
+      </section>
+
+      <section className="mb-6">
+        <h2 className="text-2xl font-semibold mb-2">2. Use of Service</h2>
+        <p>
+          Navigenius allows parents or guardians to track the location of their
+          children using a tracking device. You agree to use this Service solely
+          for its intended purpose.
+        </p>
+      </section>
+
+      <section className="mb-6">
+        <h2 className="text-2xl font-semibold mb-2">
+          3. User Responsibilities
+        </h2>
+        <p>
+          <strong>Account Creation:</strong> To access the Service, you must
+          create an account. You are responsible for maintaining the
+          confidentiality of your account information and password.
+        </p>
+        <p>
+          <strong>Accurate Information:</strong> You agree to provide accurate,
+          current, and complete information during the registration process and
+          to update such information to keep it accurate, current, and complete.
+        </p>
+        <p>
+          <strong>Device Usage:</strong> You are responsible for ensuring that
+          the tracking device is used in compliance with all applicable laws and
+          regulations, including privacy laws.
+        </p>
+      </section>
+
+      <section className="mb-6">
+        <h2 className="text-2xl font-semibold mb-2">
+          4. Privacy and Data Security
+        </h2>
+        <p>
+          We are committed to protecting your privacy. Our Privacy Policy, which
+          is incorporated into these Terms by reference, explains how we
+          collect, use, and protect your information. By using the Service, you
+          consent to the collection and use of your information as outlined in
+          our Privacy Policy.
+        </p>
+      </section>
+
+      <section className="mb-6">
+        <h2 className="text-2xl font-semibold mb-2">
+          5. Prohibited Activities
+        </h2>
+        <p>
+          You agree not to use the Service for any unlawful purpose or any
+          purpose prohibited under these Terms. You agree not to:
+        </p>
+        <ul className="list-disc list-inside ml-4">
+          <li>
+            Interfere with or disrupt the Service or servers or networks
+            connected to the Service.
+          </li>
+          <li>
+            Use the Service to transmit any content that is unlawful, harmful,
+            threatening, abusive, harassing, defamatory, vulgar, obscene, or
+            otherwise objectionable.
+          </li>
+          <li>
+            Attempt to gain unauthorized access to any part of the Service,
+            other accounts, computer systems, or networks connected to the
+            Service.
+          </li>
+        </ul>
+      </section>
+
+      <section className="mb-6">
+        <h2 className="text-2xl font-semibold mb-2">
+          6. Intellectual Property
+        </h2>
+        <p>
+          The Service and its original content, features, and functionality are
+          and will remain the exclusive property of Navigenius and its
+          licensors. The Service is protected by copyright, trademark, and other
+          laws of both the United States and foreign countries.
+        </p>
+      </section>
+
+      <section className="mb-6">
+        <h2 className="text-2xl font-semibold mb-2">7. Termination</h2>
+        <p>
+          We may terminate or suspend your account and bar access to the Service
+          immediately, without prior notice or liability, under our sole
+          discretion, for any reason whatsoever, including without limitation if
+          you breach the Terms.
+        </p>
+      </section>
+
+      <section className="mb-6">
+        <h2 className="text-2xl font-semibold mb-2">
+          8. Limitation of Liability
+        </h2>
+        <p>
+          In no event shall Navigenius, nor its directors, employees, partners,
+          agents, suppliers, or affiliates, be liable for any indirect,
+          incidental, special, consequential, or punitive damages, including
+          without limitation, loss of profits, data, use, goodwill, or other
+          intangible losses, resulting from (i) your use or inability to use the
+          Service; (ii) any unauthorized access to or use of our servers and/or
+          any personal information stored therein; (iii) any interruption or
+          cessation of transmission to or from the Service; (iv) any bugs,
+          viruses, trojan horses, or the like that may be transmitted to or
+          through our Service by any third party; and/or (v) any errors or
+          omissions in any content or for any loss or damage incurred as a
+          result of the use of any content posted, emailed, transmitted, or
+          otherwise made available through the Service.
+        </p>
+      </section>
+
+      <section className="mb-6">
+        <h2 className="text-2xl font-semibold mb-2">9. Governing Law</h2>
+        <p>
+          These Terms shall be governed and construed in accordance with the
+          laws of Philippines, without regard to its conflict of law provisions.
+        </p>
+      </section>
+
+      <section className="mb-6">
+        <h2 className="text-2xl font-semibold mb-2">
+          10. Changes to Terms and Conditions
+        </h2>
+        <p>
+          We reserve the right, at our sole discretion, to modify or replace
+          these Terms at any time. If a revision is material, we will provide at
+          least 30 days' notice prior to any new terms taking effect. What
+          constitutes a material change will be determined at our sole
+          discretion.
+        </p>
+      </section>
+
+      <section className="mb-6">
+        <h2 className="text-2xl font-semibold mb-2">11. Contact Us</h2>
+        <p>
+          If you have any questions about these Terms, please contact us at
+          navigenius1@gmail.com.
+        </p>
+      </section>
+
+      <section className="mb-6 flex justify-center">
+        <button
+          onClick={handleAgree}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold mt-4 py-2 px-4 rounded"
+        >
+          Agree
+        </button>
+      </section>
+    </div>
+  );
+};
+
+export default TermsAndConditions;
