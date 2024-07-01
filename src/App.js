241aa434aee83e48bbce4da09efa0d83d32fb719
aeolus87 <callejo.julius.07222003@gmail.com>
2024-07-01 21:44:07 +0800

    check1

--- src/App.js
+++ src/App.js
@@ -1,28 +1,46 @@
-import React from 'react';
+import React, { Suspense, lazy, useEffect, useState } from 'react';
 import { useRoutes, useLocation } from 'react-router-dom';
 import './index.css';
-import Login from './components/auth/login/index.jsx';
-import Register from './components/auth/register/index.jsx';
-import Header from './components/header/index.jsx';
-import Dashboard from './components/home/Dashboard.jsx';
-import ForgotPassword from './components/auth/forgot/index.jsx';
 import { AuthProvider } from './contexts/authContext/index.jsx';
 import PrivateRoute from './contexts/authContext/PrivateRoute.js';
 import firebase from 'firebase/compat/app';
 import firebaseConfig from './firebase/firebase.js';
-import Main from './components/main/main.jsx';
-import Profile from './components/profile/index.jsx';
-import TermsAndConditions from './components/auth/terms/TermsConditions.jsx';
-import NotFound from './components/main/notfound.jsx';
-import VerifyEmail from './components/auth/register/verifyemail.jsx';
-import VerifiedMessage from './components/auth/verify/verified.jsx';
-import CompleteRegistration from './components/auth/register/complete.jsx';
-import Emergency from './components/locationalerts/index.jsx';
 
-firebase.initializeApp(firebaseConfig);
+const Login = lazy(() => import('./components/auth/login/index.jsx'));
+const Register = lazy(() => import('./components/auth/register/index.jsx'));
+const Header = lazy(() => import('./components/header/index.jsx'));
+const Dashboard = lazy(() => import('./components/home/Dashboard.jsx'));
+const ForgotPassword = lazy(() => import('./components/auth/forgot/index.jsx'));
+const Main = lazy(() => import('./components/main/main.jsx'));
+const Profile = lazy(() => import('./components/profile/index.jsx'));
+const TermsAndConditions = lazy(
+  () => import('./components/auth/terms/TermsConditions.jsx'),
+);
+const NotFound = lazy(() => import('./components/main/notfound.jsx'));
+const VerifyEmail = lazy(
+  () => import('./components/auth/register/verifyemail.jsx'),
+);
+const VerifiedMessage = lazy(
+  () => import('./components/auth/verify/verified.jsx'),
+);
+const CompleteRegistration = lazy(
+  () => import('./components/auth/register/complete.jsx'),
+);
+const Emergency = lazy(() => import('./components/locationalerts/index.jsx'));
+
+const initializeFirebase = async () => {
+  if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+  }
+};
 
 function App() {
   const location = useLocation();
+  const [firebaseInitialized, setFirebaseInitialized] = useState(false);
+
+  useEffect(() => {
+    initializeFirebase().then(() => setFirebaseInitialized(true));
+  }, []);
 
   const routesArray = [
     { path: '/', element: <Main /> },
@@ -58,20 +76,27 @@ function App() {
 
   let routesElement = useRoutes(routesArray);
 
+  if (!firebaseInitialized) {
+    return <div>Loading...</div>; // Or some loading indicator
+  }
+
   return (
     <AuthProvider>
-      {!isTermsPage && !isMainPage && <Header />}
-      <div
-        className={`h-screen flex flex-col mx-0 my-0${
-          isTermsPage
-            ? ' bg-white'
-            : ' bg-main bg-cover bg-center backdrop-blur-sm'
-        }`}
-      >
-        {routesElement}
-      </div>
+      <Suspense fallback={<div>Loading...</div>}>
+        {!isTermsPage && !isMainPage && <Header />}
+        <div
+          className={`h-screen flex flex-col mx-0 my-0${
+            isTermsPage
+              ? ' bg-white'
+              : ' bg-main bg-cover bg-center backdrop-blur-sm'
+          }`}
+        >
+          {routesElement}
+        </div>
+      </Suspense>
     </AuthProvider>
   );
 }
 
 export default App;
+  
